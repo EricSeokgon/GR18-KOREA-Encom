16b0e3bb294d325a283b272dc1e95f129f27748c
hjchoco <73694660+hjchoco@users.noreply.github.com>
2021-06-02 11:36:56 +0900

    Add files via upload
    Code files - Hyojin: Face Recognition

---
+++ face_recognition/face_recog.py
@@ -0,0 +1,145 @@
+# face_recog.py
+
+import face_recognition
+import cv2
+import camera
+import os
+import numpy as np
+import requests
+import json
+from datetime import datetime
+
+class FaceRecog():
+    def __init__(self):
+        # Using OpenCV to capture from device 0. If you have trouble capturing
+        # from a webcam, comment the line below out and use a video file
+        # instead.
+        self.camera = camera.VideoCamera()
+
+        self.known_face_encodings = []
+        self.known_face_names = []
+        self.x = 0
+
+        # Load sample pictures and learn how to recognize it.
+        dirname = 'knowns'
+        files = os.listdir(dirname)
+        for filename in files:
+            name, ext = os.path.splitext(filename)
+            if ext == '.jpg':
+                self.known_face_names.append(name)
+                pathname = os.path.join(dirname, filename)
+                img = face_recognition.load_image_file(pathname)
+                face_encoding = face_recognition.face_encodings(img)[0]
+                self.known_face_encodings.append(face_encoding)
+
+        # Initialize some variables
+        self.face_locations = []
+        self.face_encodings = []
+        self.face_names = []
+        self.process_this_frame = True
+
+    def __del__(self):
+        del self.camera
+
+    def get_frame(self):
+        # Grab a single frame of video
+        frame = self.camera.get_frame()
+
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        # Only process every other frame of video to save time
+        if self.process_this_frame:
+            # Find all the faces and face encodings in the current frame of video
+            self.face_locations = face_recognition.face_locations(rgb_small_frame)
+            self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
+
+            self.face_names = []
+            for face_encoding in self.face_encodings:
+                # See if the face is a match for the known face(s)
+                distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+                min_value = min(distances)
+
+                # tolerance: How much distance between faces to consider it a match. Lower is more strict.
+                # 0.6 is typical best performance.
+                name = "Unknown"
+                if min_value < 0.6:
+                    index = np.argmin(distances)
+                    name = self.known_face_names[index]
+                    ''' Added test_webexbot.py here'''
+                    if self.x == 0:
+                        self.camera = camera.VideoCamera()
+                        self.send_webex_message(name)
+                        self.x = self.x+1
+                    '''until here'''
+                self.face_names.append(name)
+
+        self.process_this_frame = not self.process_this_frame
+
+        # Display the results
+        for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        return frame
+
+    def get_jpg_bytes(self):
+        frame = self.get_frame()
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        ret, jpg = cv2.imencode('.jpg', frame)
+        return jpg.tobytes()
+    
+    def send_webex_message(self, name):
+        url = "https://webexapis.com/v1/messages"
+        now = datetime.now()
+        date = datetime.date(now)
+        time = datetime.time(now)
+
+        payload = json.dumps({
+            "roomId": "Y2lzY29zcGFyazovL3VzL1JPT00vODIzODQ4ZDAtYjk0OS0xMWViLWI0Y2MtMzkwOWQ2YmQ3YTE5",
+            "text": "ALERT Missing Child " + name + " is found at " + str(date) + " " + str(time)[:8] + "  " + "Location : Lat: 35.1933, Lng: 129.0911"
+
+        })
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ODdkNDgxZGUtMTYyNS00YmQwLWIwZWEtZDY2YjBlNzlmYzk5YjRhYWE5MTEtMTg1_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print("Successfully sent a message" + response.json()['text'])
+        
+
+if __name__ == '__main__':
+    face_recog = FaceRecog()
+    print(face_recog.known_face_names)
+    while True:
+        frame = face_recog.get_frame()
+
+        # show the frame
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    print('finish')
